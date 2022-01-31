776d1a04b6142b5325eac6e53b478ed6d5f882ae
dionata correa <github.com/dionataflorindo>
2022-01-31 18:16:22 -0300

    lista

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import {v4 as uuid4} from 'uuid';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+
+
+import Tasks from './components/Tasks';
+import "./App.css";
+import AddTask from './components/AddTask';
+import Header from './components/Header';
+
+const App =()=>{
+
+// const message = "hello WORLD"
+    const[tasks, setTasks] = useState([
+        {
+            id:1,
+            title: "Estudar Programação",
+            completed: false,
+        },
+        {
+            id:2,
+            title: "Ler Livros",
+            completed: true,
+        },
+        
+    ]);
+
+    const handleTaskClick = (taskId)=>{
+        const newTasks = tasks.map((task)=>{
+            if(task.id === taskId) return {...task, completed: !task.completed}
+
+            return task;
+        })
+        setTasks(newTasks);
+    };
+
+    const handleTaskAddition = (taskTitle) =>{
+        const newTasks = [...tasks,{
+            title: taskTitle,
+            id: uuid4(),
+            completed:false, 
+
+        }];
+        setTasks(newTasks); 
+    };
+
+    const handleTaskDeletion = (taskId) => {
+        const newTasks = tasks.filter(task=> task.id != taskId) 
+
+        setTasks(newTasks)
+
+    }
+
+    return(
+        <>
+            <div className='container'>
+                <Header/>
+                <AddTask handleTaskAddition={handleTaskAddition}/>
+                <Tasks
+                    tasks={tasks}
+                    handleTaskClick={handleTaskClick}
+                    handleTaskDeletion={handleTaskDeletion}                
+                /> 
+            </div>
+        </>
+    );  
+};
+
+export default App;
